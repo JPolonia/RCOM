643b7803398ebf70dc09e63d12785e59a52f0b05
danielgranhao <up201406280@fe.up.pt>
2017-10-12 13:37:50 +0100

    modified applevel.c

--- src/applevel.c
+++ src/applevel.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
+#include <math.h>
 
 #define FALSE 0
 #define TRUE 1
 
 #define MAX_SIZE 256
 
-int numberOfOctets(int number){
+int int_pow(int base, int exp){ // funciona
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+            result *= base;
+        exp /= 2;
+        base *= base;
+    }
+    return result;
+}
+
+int numberOfOctets(int number){  //funciona
     int n = 0;
     while (number != 0) {
         number >>= 8;
@@ -17,11 +31,11 @@ int numberOfOctets(int number){
     return n;
 }
 
-int controlPacket(char *buffer, int fileSize, char *fileName, char C){
+int controlPacket(char *buffer, int fileSize, char *fileName, char C){  //funciona acho :P
     int i = 0;
     int j = 0;
     int exp = 0;
-    int sum = 0;
+    int nOctets = 0;
     
     
     buffer[i] = C; //start/end control flag
@@ -30,16 +44,17 @@ int controlPacket(char *buffer, int fileSize, char *fileName, char C){
     buffer[i] = 0x00; //file size flag
     i++;
     
-    buffer[i] = (char) numberOfOctets(fileSize); //file size number of bytes
+    nOctets = numberOfOctets(fileSize);
+    buffer[i] = (char) nOctets; //file size number of bytes
     i++;
     
-    exp = 0;
-    sum = 0;
-    for(j = 0 ; j < numberOfOctets(fileSize) ; j++){
-        buffer[i] = (char)( (fileSize & (2^(exp+8) - sum)) >> exp);// anda de 8 em 8 bits e arrasta para a direita;
-        exp = exp + 8;
+    exp = 8 * nOctets;
+    for(j = 0 ; j < nOctets ; j++){ //OK
+        buffer[i] = (char)( (fileSize & (int_pow(2, exp)-1)   ) >> (exp-8));// anda de 8 em 8 bits e arrasta para a direita;
+        //printf("\n%d\n", buffer[i]);
+        
+        exp = exp - 8;
         i++;
-        sum = sum + 2^exp;
     }
     
     buffer[i] = 0x01; //file name flag
@@ -58,11 +73,11 @@ int controlPacket(char *buffer, int fileSize, char *fileName, char C){
     
 }
 
-int startPacket(char *buffer, int fileSize, char *fileName){
+int startPacket(char *buffer, int fileSize, char *fileName){ //funciona
     return controlPacket(buffer, fileSize, fileName, 0x02);
 }
 
-int endPacket(char *buffer, int fileSize, char *fileName){
+int endPacket(char *buffer, int fileSize, char *fileName){  //funciona
     return controlPacket(buffer, fileSize, fileName, 0x03);
 }
 
@@ -87,5 +102,38 @@ int dataPacket(char *buffer, int seqNumber, int tamanho, char *data ){
     }
     return i;
 }
+
+FILE *openFile(char *pathToFile){ //falta verificar
+    FILE *file = fopen(pathToFile, "rb");
+    if (file == NULL) {
+        printf("Erro ao abrir ficheiro\n");
+        return NULL;
+    }
+    return file;
+}
+
+size_t sendData(FILE *file, int fd){ //falta verificar
+    char buffer[MAX_SIZE];
+    size_t bytesRead = 0;
+    size_t total = 0;
+    while(bytesRead = fread(buffer, 1, MAX_SIZE, file), bytesRead != 0){
+        //llwrite(fd, buffer, bytesRead);
+        total = total + bytesRead;
+    }
+    return total;
+}
+
+void closeFile(FILE *file){
+    fclose(file);
+}
+
+
+    
+    
+    
+    
+
+
+
 
 
