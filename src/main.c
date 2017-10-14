7487ebe43a9590a01835f98218d60daaccf50df5
Daniel <daniel.granhao@gmail.com>
2017-10-14 15:04:05 +0100

    testing

--- src/main.c
+++ src/main.c
@@ -5,98 +5,65 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "linklayer.h"
 #include "alarm.h"
 #include "applevel.h"
-#include "linklayer.h"
 
-//#include <sys/types.h>
-//#include <sys/stat.h>
-//#include <fcntl.h>
 
-//#define BAUDRATE B38400
 
-//#define MAX_TRIES 3
-//#define TIMEOUT 3
+#define BAUDRATE B38400
 
-//linkLayer* ll;
+#define MAX_TRIES 3
+#define TIMEOUT 3
+
+linkLayer* ll;
 
 int main(int argc, char** argv)
 {
-    //FILE *f = openFileTransmmiter("src/applevel.c");
-    //sendData(f, 4);
-    
-    //FILE *f = openFileReceiver("src/teste.txt");
-    //printf("TOTAL = %d\n", receiveData(f, 4)  );
- 
-    
-    //char fileName[20];
-    //int tamanho = receiveStart(4, fileName);
-    //printf("tamanho = %d \nname = %s \n", tamanho, fileName);
-    
-    
-    //exemplo de utilização RECEIVER::::_____________________________
-    
-    
-    
-    /*
-    int fileSize;
-    char fileName[];
-    FILE *f;
-    bytesReceived = 0;
-    
-    fileSize = receiveStart(fd, fileName); //espera por START e guarda tamanho e nome
-    
-    f = openFileReceiver(fileName); //cria ficheiro
-    
-    bytesReceived = receiveData(f, fd); //recebe e escreve dados
-    
-    if(bytesReceived == fileSize){
-        printf("OK!!");
-    }
-    else{
-        printf("NOT OK...");
-    }
-     
-    fclose(f);
-    */
-    
-    
-    
-    //termina exemplo_________________________________________________
-    
-    //exemplo de utilização TRANSMITTER::::___________________________
-    
-    
-    
-    
-    /*
-    int fileSize;
-    char fileName[] = "o caminho para o ficheiro fica aqui :)";
-    int bytesTransmitted = 0;
-    
-    FILE *f = openFileTransmmiter(fileName); //abre ficheiro
-    
-    fseek(f, 0L, SEEK_END); //obtem tamanho do ficheiro
-    fileSize = ftell(f);
-    rewind(f);
-    
-    sendStartPacket(fd, fileSize, fileName);
-    
-    sendData(f, fd);
-    
-    sendEndPacket(fd, fileSize, fileName);
-    
-    fclose(f);
-    */
-    
-    
-    
-    //termina exemplo_________________________________________________
-    
+    int fd, e;
+    char mode;
+    char file_name[] = "ola";
+    char read_str[255];
+    //int fd_file;  
 	
 
+    if ( (argc < 3) || 
+         ((strcmp("/dev/ttyS0", argv[1])) && (strcmp("/dev/ttyS1", argv[1])) ) ||
+         ((strcmp("TRANSMITTER", argv[2])) && (strcmp("RECEIVER", argv[2])) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1 MODE\n");
+      exit(1);
+    }
+
+
+    //Initialize linkLayer
+    e = initLinkLayer(argv[1],BAUDRATE,0,TIMEOUT,MAX_TRIES);
+    if (e <0) { printf("Couldnt initialize linklayer\n");; exit(-1); }
+
+    //Open Serial Port
+    fd = openSerialPort(ll->port);
+    if (fd <0) {perror(ll->port); exit(-1); }
+
+    //Initialize Termios
+    e = initTermios(fd);
+    if (e <0) { printf("Couldnt initialize Termios\n");; exit(-1); }
+
+    //Initialize alarm
+	initAlarm();
 
     
+    mode = (strcmp("TRANSMITTER", argv[2])) ? RECEIVER : TRANSMITTER;
+    llopen(fd, mode);
+	/*
+    switch(mode){
+        case TRANSMITTER: llwrite(fd, file_name,strlen(file_name));
+                          break;
+        case RECEIVER:    llread(fd,read_str);
+                          printf("STR: %s \n",read_str);
+                          break;
+      }
+	*/    
 
-    
+    sleep(1);
+   
+    return 0;
 }
